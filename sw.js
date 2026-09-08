4ecb57fb65b56bea024e7a77093c773ffa9909c2
ozeias-m <ozeiasarierom@gmail.com>
2026-09-08 13:51:06 -0300

    Atualizar cache para navegador

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "localizador-caixas-v2";
+const CACHE_NAME = "localizador-caixas-v3";
 const APP_FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -18,6 +18,12 @@ self.addEventListener("activate", (event) => {
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request).then((response) => {
+      if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match(event.request))
   );
 });
