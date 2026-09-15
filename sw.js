d99d6a967d479e934b66d58b45e52234201a68f8
ozeias-m <ozeiasarierom@gmail.com>
2026-09-15 07:45:06 -0300

    Corrige exclusão das réguas da conferência

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "localizador-caixas-v4";
+const CACHE_NAME = "localizador-caixas-v5";
 const APP_FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
